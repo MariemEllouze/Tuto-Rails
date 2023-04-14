2d1a749afdae01d8727a7bb924c25a8c30e568d7
MariemEllouze <mariem.sastec@gmail.com>
2023-04-14 11:26:49 +0200

    designed form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,25 +15,26 @@ class QuotesController < ApplicationController
     end
   
     def create
-      @quote = Quote.new(quote_params)
-  
-      if @quote.save
-        redirect_to quotes_path, notice: "Quote was successfully created."
-      else
-        render :new
+        @quote = Quote.new(quote_params)
+    
+        if @quote.save
+          redirect_to quotes_path, notice: "Quote was successfully created."
+        else
+          # Add `status: :unprocessable_entity` here
+          render :new, status: :unprocessable_entity
+        end
       end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @quote.update(quote_params)
-        redirect_to quotes_path, notice: "Quote was successfully updated."
-      else
-        render :edit
+    
+      # ...
+    
+      def update
+        if @quote.update(quote_params)
+          redirect_to quotes_path, notice: "Quote was successfully updated."
+        else
+          # Add `status: :unprocessable_entity` here
+          render :edit, status: :unprocessable_entity
+        end
       end
-    end
   
     def destroy
       @quote.destroy
