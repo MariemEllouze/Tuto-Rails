0d1d7988dc2123dd90c50f0198a3a3c64cfd7f26
MariemEllouze <mariem.sastec@gmail.com>
2023-04-20 13:48:36 +0200

    fix problem of destroy

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -5,7 +5,7 @@ class LineItemDate < ApplicationRecord
   validates :date, presence: true, uniqueness: { scope: :quote_id }
 
   scope :ordered, -> { order(date: :asc) }
-  def previous_date
+   def previous_date
     quote.line_item_dates.ordered.where("date < ?", date).last
   end
 end
