6018ab4a7b4cc3393b386a5875d1918951c11a74
MariemEllouze <mariem.sastec@gmail.com>
2023-04-20 11:48:58 +0200

    chapter 10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
     def render_turbo_stream_flash_messages
       turbo_stream.prepend "flash", partial: "layouts/flash"
     end
+
     def form_error_notification(object)
       if object.errors.any?
         tag.div class: "error-message" do
@@ -9,4 +10,9 @@ module ApplicationHelper
         end
       end
     end
+
+    def nested_dom_id(*args)
+      args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
+    end
+    
   end
